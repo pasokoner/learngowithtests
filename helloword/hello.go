c814aa2ed5935a58ee4e64370a1ad929c992412c
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-07 15:45:30 +0800

    hello world test

---
+++ helloword/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
