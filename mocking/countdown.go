9ede434f9c40fb21ba68df4742702611f420c3f3
John Carlo Asilo <jcarloasilo@gmail.com>
2024-05-12 11:06:04 +0800

    add mock countdown

---
+++ mocking/countdown.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, "Go!")
+}
+
+func main() {
+	Countdown(os.Stdout, &DefaultSleeper{})
+}
